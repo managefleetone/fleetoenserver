c8454062d294d7372f739054c373353c58faa5eb
managefleetone <fleetone.manage@gmail.com>
2025-03-25 20:38:34 +0400

    sefrsfsfw

--- render-build.sh
+++ render-build.sh
@@ -3,5 +3,4 @@
 # Установка всех зависимостей проекта
 npm install
 
-# Просто проверка наличия установленного пакета @sparticuz/chromium
-ls -l node_modules/@sparticuz/chromium/
+npx playwright install chromium
