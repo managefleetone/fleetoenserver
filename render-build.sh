75f0b9c6fb01f867501d869ccb8a425421963d3a
managefleetone <fleetone.manage@gmail.com>
2025-03-25 18:22:51 +0400

    awawawa

--- render-build.sh
+++ render-build.sh
@@ -3,8 +3,5 @@
 # Установка всех зависимостей проекта
 npm install
 
-# Установка браузеров Playwright с зависимостями
-npx playwright install chromium --with-deps
-
 # Проверка наличия установленного браузера
 ls -l /opt/render/project/.cache/ms-playwright/chromium/chrome-linux/
