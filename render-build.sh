d32832a788706e592fb4d4abac5c6bc8c3fc96ab
managefleetone <fleetone.manage@gmail.com>
2025-03-25 18:07:39 +0400

    wwwwww

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Устанавливаем браузер с зависимостями, используя Playwright
+# Установка всех зависимостей проекта
+npm install
+
+# Установка браузеров Playwright с зависимостями
 npx playwright install chromium --with-deps
 
-# Проверка на наличие установленного браузера
+# Проверка наличия установленного браузера
 ls -l /opt/render/project/.cache/ms-playwright/chromium/chrome-linux/
