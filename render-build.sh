cf3862b9ab72be6165c979c5679d733f3a3a4fd9
managefleetone <fleetone.manage@gmail.com>
2025-03-25 17:08:58 +0400

    e

--- render-build.sh
+++ render-build.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
-# Установка необходимых системных библиотек
-apt-get update && apt-get install -y \
-  libnss3 \
-  libatk1.0 \
-  libatk-bridge2.0 \
-  libgtk-3-0 \
-  libasound2 \
-  libxcomposite1 \
-  libxdamage1 \
-  libxrandr2 \
-  libgbm1
-
-# Установка Node.js-зависимостей и браузеров Playwright
-echo "Installing dependencies..."
+# Установка npm-зависимостей
 npm install
-echo "Installing Playwright browsers..."
-npx playwright install --with-deps chromium
+
+# Загрузка браузеров Playwright в кэш Render без использования root-доступа
+echo "Installing Playwright browsers in user space..."
+npx playwright install chromium
+
