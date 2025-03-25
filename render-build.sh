b9afa66d9f44ba80b1387a8d3602d0813a72015d
managefleetone <fleetone.manage@gmail.com>
2025-03-25 17:47:34 +0400

    k

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Устанавливаем переменную окружения для пути браузеров
-export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/.cache/ms-playwright
-
-# Установка только браузера с помощью playwright
-npx playwright install chromium --with-deps
+# Установка Chromium с помощью apt
+sudo apt-get update
+sudo apt-get install chromium -y
 
 # Проверка на наличие браузера
-ls -l /opt/render/project/.cache/ms-playwright/chromium/chrome-linux/
+which chromium
