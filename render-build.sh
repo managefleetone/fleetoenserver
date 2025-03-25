b436403e20e1f673259025a9f96ef89eed54b4c2
managefleetone <fleetone.manage@gmail.com>
2025-03-25 18:28:24 +0400

    qqqaaqqqqqaqqq

--- render-build.sh
+++ render-build.sh
@@ -3,5 +3,5 @@
 # Установка всех зависимостей проекта
 npm install
 
-# Просто проверка наличия папки с браузером, специфичной для @sparticuz/chromium
-ls -l /opt/render/project/node_modules/@sparticuz/chromium/
+# Просто проверка наличия установленного пакета @sparticuz/chromium
+ls -l node_modules/@sparticuz/chromium/
